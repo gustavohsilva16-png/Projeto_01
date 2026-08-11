21708de13cc9ebd34d3e0baac1f7dc740389a33a
gustavohsilva16-png <gustavo.h.silva16@edu.senai.br>
2026-08-11 14:22:19 -0300

    Create variaveis.js

---
+++ variaveis.js
@@ -0,0 +1,55 @@
+//let = valor pode ser alterado
+//    utilizado para atualizações
+//    ideal para contadores e loops
+
+// //const = valor é fixo - não pode ser alterado 
+//     mais seguro e previsível 
+//     prefido pelas empresas
+
+// var = escopo confuso 
+//     pode ser redeclarado
+//     gera bugs silenciosos
+ 
+// não usar o var
+
+const nomeAluno = "Dimenos"; //certo
+const a = "Dimenos"; //ruim
+
+let x = 15; //ruim
+let quantidadedeAlunos = 15; //certo
+
+// Tipos de dados 
+
+// String
+// "joão"
+
+// Number
+// 20 ou 1.80
+
+// Boolean
+// true ou false
+
+// undefined
+// let x ;
+
+// null
+// const telefone = null
+
+let nome = "diana"
+console.log(nome);
+
+console.log("Olá " + nome)
+
+let cidade  = "osasco"
+let estado = "SP"
+
+console.log(cidade + " - " + estado)
+console.log("`${cidade} - ${estado}`")
+
+let numero = 10
+console.log(numero)
+
+
+const idade = 16 // number
+const matricula = true // Boolean
+const tel = null // Null

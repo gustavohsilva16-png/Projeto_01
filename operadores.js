2e62d3f924409e67e6f45f2adf44b9f8fe5e86b2
gustavohsilva16-png <gustavo.h.silva16@edu.senai.br>
2026-08-11 14:19:55 -0300

    Create operadores.js with operator examples
    Add basic arithmetic and comparison operators examples in operadores.js

---
+++ operadores.js
@@ -0,0 +1,34 @@
+// soma + 
+// subtração -
+// multiplicação *
+// Divisão / 
+// resto (modulo) %
+// potência ** 2 ** 3 = 8
+
+// Se n % == 0 , o numero é par 
+
+// > maior que
+// < menor que
+// >= maior ou igual a
+// <= menor ou igual a
+// == igual
+// === estritamente igual
+// != diferete 
+// !== estritamente diferente
+
+
+let idade = 20
+console.log(idade > 10)
+
+console.log(10>15)
+
+let nota = 6
+
+console.log(nota < 7)
+console.log(25 >= 10)
+console.log(idade >= 18)
+
+let temperatura = 10
+console.log(temperatura <= 10)
+
+console.log(10 === "10")
